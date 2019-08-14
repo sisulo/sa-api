5781b7ab1407ab49f7ec202942576a75d6f0d0ca
Ivan Sisak <ivo.sisak@gmail.com>
2019-08-15 00:53:54 +0200

    Bug #232 - New logical metric types

---
+++ src/migration/1565799851764-AddNewMetricType.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddNewMetricType1565799851764 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    queryRunner.query(
+        `INSERT INTO cat_metric_type (id_cat_metric_type, name, unit, id_cat_metric_group)
+                  VALUES(37, 'SUBSCRIBED_CAPACITY', 'TB', 2),
+                  (38, 'LOGICAL_SUBS_PERC', '%', 2),
+                  (39, 'NET_SUBS_PERC', '%', 2);`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+  }
+
+}
