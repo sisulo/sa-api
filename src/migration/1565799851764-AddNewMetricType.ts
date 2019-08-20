ceb0b5a253d10758720f7d073fe7d9768b58d5bd
sisulo <ivo.sisak@gmail.com>
2019-08-20 13:27:04 +0200

    Update 1565799851764-AddNewMetricType.ts

--- src/migration/1565799851764-AddNewMetricType.ts
+++ src/migration/1565799851764-AddNewMetricType.ts
@@ -7,7 +7,7 @@ export class AddNewMetricType1565799851764 implements MigrationInterface {
         `INSERT INTO cat_metric_type (id_cat_metric_type, name, unit, id_cat_metric_group)
                   VALUES(37, 'SUBSCRIBED_CAPACITY', 'TB', 2),
                   (38, 'LOGICAL_SUBS_PERC', '%', 2),
-                  (39, 'NET_SUBS_PERC', '%', 2)
+                  (39, 'NET_SUBS_PERC', '%', 2),
                   (40, 'NET_USED_PERC', '%', 2);`,
     );
   }
