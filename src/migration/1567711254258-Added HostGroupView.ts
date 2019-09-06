df7fda2f76553c2a02058f14b0554f706d0a3be7
Ivan Sisak <ivo.sisak@gmail.com>
2019-09-06 19:26:34 +0200

    Changed seq values to last

--- src/migration/1567711254258-Added HostGroupView.ts
+++ src/migration/1567711254258-Added HostGroupView.ts
@@ -19,7 +19,11 @@ export class AddedHostGroupView1567711254258 implements MigrationInterface {
                FROM host_group_metrics inner_sm
               WHERE outer_sm.id_cat_metric_type = inner_sm.id_cat_metric_type AND outer_sm.id_host_group = inner_sm.id_host_group
               ORDER BY inner_sm.date DESC
-             LIMIT 1));`);
+             LIMIT 1));
+      SELECT setval('pools_id_pool_seq', (SELECT MAX(id_pool) from pools));
+      SELECT setval('systems_id_system_seq', (SELECT MAX(id_system) from systems));
+      SELECT setval('chas_id_cha_seq', (SELECT MAX(id_cha) from chas));
+`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
