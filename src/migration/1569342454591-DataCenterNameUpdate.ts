0d3b9b84b284f9cd2817ce509b7ae60dbf6bde80
sisulo <ivo.sisak@gmail.com>
2019-09-24 18:39:10 +0200

    #262 - change database names

---
+++ src/migration/1569342454591-DataCenterNameUpdate.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class DataCenterNameUpdate1569342454591 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    queryRunner.query(`
+        UPDATE datacenters
+SET name='MY_Cyberjaya'
+WHERE id_datacenter=3;
+        `);
+    queryRunner.query(`UPDATE datacenters
+SET name='US_Mechanicsburg'
+WHERE id_datacenter=6;`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+  }
+
+}
