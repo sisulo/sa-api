a4e1a6dd392dfff5d8c644fc6373cf24e3018683
Ivan Šišák <ivo.sisak@gmail.com>
2020-06-01 22:00:09 +0200

    #389 - materialized view

--- src/migration/1590697019456-MaterializedViews.ts
+++ src/migration/1590697019456-MaterializedViews.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 export class MaterializedViews1590697019456 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<any> {
-    return await queryRunner.query(fs.readFileSync(__dirname + '/../../database/init.sql').toString());
+    return await queryRunner.query(fs.readFileSync(__dirname + '/../../database/views.sql').toString());
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
